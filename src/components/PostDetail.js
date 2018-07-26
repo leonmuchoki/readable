f9c61b02d21f53548b53bb8cb921f5c849257b82
Leon Muchoki <muchokileon@gmail.com>
2018-07-26 19:02:02 +0300

    feat: add delete and edit of posts in post details component

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import * as ReadableAPI from '../utils/ReadableAPI';
-//import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Loading from 'react-loading';
+import { Link } from 'react-router-dom';
+import EditIcon from 'react-icons/lib/fa/pencil';
+import DeleteIcon from 'react-icons/lib/fa/trash';
 
 import PostComment from './PostComment';
 import PostVote from './PostVote';
@@ -12,6 +14,7 @@ import { getPostData } from '../actions/posts';
 import CommentsContainer from '../containers/CommentsContainer';
 import * as Helpers from '../utils/helpers';
 import NotFound from './NotFound'
+import { postDelete } from '../actions/posts';
 
 class PostDetail extends Component {
   state = {
@@ -22,6 +25,7 @@ class PostDetail extends Component {
     const id = this.props.match.params.id
     this.getPostDetails(id);
   }
+  
 
   getPostDetails = (id) => {
     this.props.fetchData(id)
@@ -34,6 +38,11 @@ class PostDetail extends Component {
     return false
   }
 
+  delPost = (postId) => {
+    this.props.deletePost(postId)
+    this.props.history.push('/')
+  }
+
   render () {
     //const categories = this.props.categories
     const post_details = this.props.postDetails
@@ -42,6 +51,7 @@ class PostDetail extends Component {
     const comment_count = post_details["commentCount"]
     const vote_count = post_details["voteScore"]
     const post_id = this.props.match.params.id
+    let deletePost = this.props.deletePost
     console.log('postdetails...' + JSON.stringify(post_id))
 
     return (
@@ -60,6 +70,14 @@ class PostDetail extends Component {
               <div className="post-detail-footer">
                 <PostComment countComments={comment_count} />
                 <PostVote countVotes={vote_count} postId={post_id} />
+
+                <span aria-hidden="true" className="bullet"> · </span>
+                <Link to={`/post/edit/${ post_details["id"] }`}>
+                  <span className="posts-delete-icon" ><EditIcon /></span>
+                </Link>
+
+                <span aria-hidden="true" className="bullet"> · </span>
+                <span className="posts-delete-icon" onClick={()=>this.delPost(post_details["id"])}><DeleteIcon /></span>
               </div>
               <div>
                   <CommentsContainer postId={post_id} />
@@ -90,7 +108,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchData: (postId) => dispatch(getPostData(postId))
+    fetchData: (postId) => dispatch(getPostData(postId)),
+    deletePost: (postId) => dispatch(postDelete(postId))
   }
 }
 
