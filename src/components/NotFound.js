d63884c5b142348c341ea7d2a18efb6b95ca2c90
Leon Muchoki <muchokileon@gmail.com>
2018-07-26 08:15:23 +0300

    feat: show link to home in NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import HomeIcon from 'react-icons/lib/fa/home';
 
 class NotFound extends Component {
   render() {
     return (
       <div>
         <h4>Error 404 - Post not found.</h4>
+        <Link to="/" className="btn-add-post"><HomeIcon /> Home</Link>
       </div>
     )
   }
